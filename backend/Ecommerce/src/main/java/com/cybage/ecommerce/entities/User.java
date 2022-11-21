687ffa1437d916e332498e7fe7d4fa57d9575c3e
Rupal27632 <111897713+Rupal27632@users.noreply.github.com>
2022-11-21 17:10:29 +0530

    MajorProject2022

---
+++ backend/Ecommerce/src/main/java/com/cybage/ecommerce/entities/User.java
@@ -0,0 +1,78 @@
+package com.cybage.ecommerce.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "tb_user", uniqueConstraints = @UniqueConstraint(columnNames = "emailId"))
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "emailId")
+	private String emailId;
+	
+	@Column(name = "number")
+	private String number;
+	
+	@Column(name = "password")
+	private String password;
+
+	public User() {}
+	public User(String name, String emailId, String number, String password) {
+		super();
+		this.name = name;
+		this.emailId = emailId;
+		this.number = number;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ backend/Ecommerce/src/main/java/com/cybage/ecommerce/entities/Vendor.java
@@ -0,0 +1,10 @@
+package com.cybage.ecommerce.entities;
+
+public class Vendor {
+	private Long Id;
+	private String name;
+	private String panNumber;
+	private String gstNumber;
+	private Long AddressId;
+	private String accountNumber;	
+}
